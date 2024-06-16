c27ec2c77f84038cbcf77ff77da17be074e7847c
ftorn1k <154966484+ftorn1k@users.noreply.github.com>
2024-06-16 17:12:45 +0300

    Страница заявки

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -21,5 +21,6 @@ export default function AppLayout(props:any){
             {props.children}
         </main>
         </div>
+        
     </div>
 }
--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Home() {
   return (
-    <div>
-      <p>Привет</p>
-    </div>
+    <div  className="p-6">
+    <Table className="max-w-7xl">
+  <TableHeader className="bg-[#ffd600]">
+    <TableRow>
+      <TableHead className="w-80 text-black">Название компании</TableHead>
+      <TableHead className="text-black">Описание</TableHead>
+      <TableHead className="text-right w-48 text-black">Статус</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">Название компании</TableCell>
+      <TableCell>Краткое описание</TableCell>
+      <TableCell className="text-right">Статус</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">Название компании</TableCell>
+      <TableCell>Краткое описание</TableCell>
+      <TableCell className="text-right">Статус</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">Название компании</TableCell>
+      <TableCell>Краткое описание</TableCell>
+      <TableCell className="text-right">Статус</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+</div>
+    
   );
 }
