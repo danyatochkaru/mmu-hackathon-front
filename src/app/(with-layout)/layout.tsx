81220d9ed92c5fa5a04372f3e499257a90f970c1
Daniil <support@danyatochka.ru>
2024-06-16 16:19:58 +0300

    feat: небольшие улучшения и корректировки кода

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -1,25 +1,41 @@
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
+import {PropsWithChildren} from "react";
+import Link from "next/link";
 
-export default function AppLayout(props:any){
+export default function AppLayout(props: PropsWithChildren) {
+    const name = "Иванов Иван Иванович"
+    const email = "i.i.i@mail.ru"
+    const links = [
+        {name: "Заявки", href: "/"},
+        {name: "Создать заявку", href: "/"},
+        {name: "Статистика и отчетность", href: "/"},
+    ]
+
     return <div className="flex">
-       
-        <nav className="w-80 border-r-4 h-dvh bg-[#C0C0C0]" >
+        <nav className="w-80 border-r-4 h-dvh bg-[#C0C0C0]">
             <div className="bg-[#ffd600] p-4">
-                <p>Иванов Иван Иванович</p>
-                <span>i.i.i@mail.ru</span>
+                <h4 className="scroll-m-20 text-md font-semibold tracking-tight">
+                    <Link href={'/profile'}>{name}</Link>
+                </h4>
+                <p className="text-sm text-muted-foreground"><Link href={"/profile"}>{email}</Link></p>
             </div>
-            <div className="flex flex-col py-2 gap-y-2">
-            <Button className="bg-[#FFFFFF] text-[#000]" variant={"secondary"}>Заявки</Button> 
-            <Button className="bg-[#FFFFFF] text-[#000]" variant={"secondary"}>Создать заявку</Button>
+            <div className="flex flex-col px-1 py-2 gap-y-2">
+                {links.map(link => (
+                    <Link key={link.name} href={link.href}>
+                        <Button className="bg-[#FFFFFF] w-full justify-start">
+                            {link.name}
+                        </Button>
+                    </Link>
+                ))}
             </div>
         </nav>
         <div className="w-full">
-        <header className="p-3 border-b-2">
-            <span className="text-lg font-semibold">Заявки</span>
-        </header>
-        <main>
-            {props.children}
-        </main>
+            <header className="p-3 border-b-2">
+                <span className="text-lg font-semibold">Заявки</span>
+            </header>
+            <main>
+                {props.children}
+            </main>
         </div>
     </div>
 }
--- src/app/(with-layout)/profile/page.tsx
+++ src/app/(with-layout)/profile/page.tsx
@@ -1,24 +1,22 @@
 ﻿import * as React from "react"
 
 type profileTypes = "student" | "company" | "rop" | "admin"
-let currentProfileType = "company"
+let currentProfileType: profileTypes = "company"
 
 let fullName = "Иванов Иван Иванович"
 let email = "example@example.com"
 let group = "ИПС311-1"
 let companyName = "ООО \"Название компании\""
 
-export default function Profile(){
+export default function Profile() {
     return (
         <div>
             <p>ФИО: {fullName}</p>
             <p>Почта: {email}</p>
-
             {
                 currentProfileType === "student" && <p>Группа: {group}</p> ||
                 currentProfileType === "company" && <p>Название компании: {companyName}</p>
             }
-            
         </div>
     )
 }
