2113da701b9c2323e8b482d4e889a59e9e0a465d
Son1kXDev <son1kx@vk.com>
2024-06-16 15:18:09 +0300

    feat: страница профиля и статистики

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react"
+
+let studentsCount = 156;
+let requestCount = 176;
+
+export default function Stats(){
+    return (
+        <div>
+            <p>Количество студентов: {studentsCount}</p>
+            <p>Количество заявок: {requestCount}</p>
+        </div>
+    )
+}
