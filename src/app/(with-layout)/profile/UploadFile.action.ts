82581cf8a52954a966bf1a3cef9ae2d8aa99b564
Daniil <support@danyatochka.ru>
2024-06-18 17:48:13 +0300

    feat: рабочая загрузка cv

--- src/app/(with-layout)/profile/UploadFile.action.ts
+++ src/app/(with-layout)/profile/UploadFile.action.ts
@@ -4,6 +4,7 @@ import {auth} from "@/lib/auth";
 import {ROUTES} from "@/constants/routes";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import qs from "qs";
 
 export default async function UploadFileAction(formData: FormData) {
     const session = await auth()
@@ -12,11 +13,10 @@ export default async function UploadFileAction(formData: FormData) {
 
     preparedData.append('files', formData.get('file')!)
 
-    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/upload`, {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/upload`, {
         method: "POST",
         body: preparedData,
         headers: {
-            "Content-Type": "multipart/form-data",
             Authorization: `Bearer ${session?.user.token}`
         },
     })
@@ -25,6 +25,39 @@ export default async function UploadFileAction(formData: FormData) {
             console.error(err)
         })
 
+    const studentData = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/students?${qs.stringify({
+        populate: '*',
+        filter: {
+            users_permissions_user: {
+                id: {
+                    '$eq': session?.user.id
+                }
+            }
+        }
+    })}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user.token}`
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error(err)
+        })
+
+    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/students/${studentData.data[0].id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user.token}`
+        },
+        body: JSON.stringify({
+            data: {
+                cv_file: data[0].id
+            }
+        })
+    })
 
     const route = ROUTES.profile + `?successMessage=${encodeURIComponent('CV файл загружен.')}`
     revalidatePath(route)
